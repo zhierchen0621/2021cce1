06c0b2a938c02131aa6eb813088777f4e58a9a28
zhierchen0621 <91171547+zhierchen0621@users.noreply.github.com>
2021-11-24 11:47:21 +0800

    Add files via upload

---
+++ week11/week11-1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int a[10000000];
+int main()
+{
+    int m;
+    scanf("%d",&m);
+
+    for (int i=2;i<m;i++){
+        if (a[i]==0){
+            printf("%d ",i);
+            for (int k=i+i;k<m;k+=i){
+                a[k]=1;
+            }
+        }
+    }
+}
